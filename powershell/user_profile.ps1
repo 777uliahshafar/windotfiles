0d315ac181814e47a705b25982cccc2438f13f94
unknown <777uliahshafar@gmail.com>
2023-06-01 15:34:58 +0800

    powershellconfig

---
+++ powershell/user_profile.ps1
@@ -0,0 +1,30 @@
+#Prompt
+Import-Module posh-git
+#oh-my-posh init pwsh | Invoke-Expression # it needs to be uncomment for first time
+$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -PredictionSource History
+
+# Fzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Alias 
+Set-Alias ll ls
+Set-Alias g git 
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+# Utilities
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
