ec3fb4d965e1f966bda29c3aafae2fd14e39dd29
777uliahshafar <777uliahshafar@gmail.com>
2023-06-30 05:47:17 +0700

    newterminal0701

--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,22 +1,61 @@
-#Prompt
+function Run-Step([string] $Description, [ScriptBlock]$script)
+{
+  Write-Host  -NoNewline "Loading " $Description.PadRight(20)
+  & $script
+  Write-Host "`u{2705}" # checkmark emoji
+}
+
+$light = Start-ThreadJob {
+Import-Module -Name z
 Import-Module posh-git
-#oh-my-posh init pwsh | Invoke-Expression # it needs to be uncomment for first time
-$omp_config = Join-Path $PSScriptRoot ".\takuya.omp.json"
-oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
-Import-Module -Name Terminal-Icons
+$env:POSH_GIT_ENABLED = $true
+}
 
-# PSReadLine
+
+$PSReadLine = Start-ThreadJob {
 Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineOption -EditMode Windows
 Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineKeyHandler -Function AcceptSuggestion -Key 'Ctrl+Spacebar'
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward
+Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward 
 
-# Fzf
+
+Set-PSReadlineOption -Color @{
+    "Command"          = [ConsoleColor]::Green
+    "Parameter"        = [ConsoleColor]::Gray
+    "Operator"         = [ConsoleColor]::Magenta
+    "Variable"         = [ConsoleColor]::Yellow
+    "String"           = [ConsoleColor]::Yellow
+    "Number"           = [ConsoleColor]::Yellow
+    "Type"             = [ConsoleColor]::Cyan
+    "Comment"          = [ConsoleColor]::DarkCyan
+    "InlinePrediction" = '#70A99F'
+  }
+}
+
+$PSFzf = Start-ThreadJob {
 Import-Module PSFzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+}
+
+Run-Step "oh-my-posh" {
+$omp_config = Join-Path $PSScriptRoot ".\jondjones.omp.json"
+oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
+}
+
+Run-Step "terminal-icons" {
+Import-Module -Name Terminal-Icons
+}
+
+Receive-Job $PSReadLine -Wait -AutoRemoveJob
+Receive-Job $PSFzf -Wait -AutoRemoveJob
+Receive-Job $light -Wait -AutoRemoveJob
 
 # Alias 
+Set-Alias v nvim
 Set-Alias ll ls
 Set-Alias g git 
 Set-Alias grep findstr
