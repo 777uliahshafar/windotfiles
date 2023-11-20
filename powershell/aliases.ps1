5ed20dea1b4727d3544003d72b7db976579fe6b7
Uliah Shafar <67497706+777uliahshafar@users.noreply.github.com>
2023-11-20 10:40:16 +0800

    novaliases

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -11,12 +11,30 @@ function nvimalias
     & 'nvim' '.\aliases.ps1'
 }
 
-function phdjournal
+function vimmyhelp
 {
-    cd '~/Documents/phdjournal/'
-    & 'nvim' '.\phdjournal.tex'
+    cd '~/vim-myhelp/doc'
+    & 'nvim' '.\mycheat.txt'
 }
 
+
+function statistika
+{
+    cd 'D:\knstatistika'
+    & 'nvim' '.\main.tex'
+}
+
+function interviewbpi
+{
+    cd 'D:\interviewbpi'
+    & 'nvim' '.\interview.tex'
+}
+
+function microteaching
+{
+    cd 'D:\microteach'
+    & 'nvim' '.\microteach.tex'
+}
 
 # Alias
 Set-Alias v nvim
@@ -27,6 +45,12 @@ Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
 Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias nc nvimconfig
 Set-Alias na nvimalias
+Set-Alias nvc vimmyhelp
 Set-Alias jot neorgnotes
 
-Set-Alias phd phdjournal
+# Set-Alias phd phdjournal
+Set-Alias sta statistika
+Set-Alias int interviewbpi
+
+Set-Alias mt microteaching
+
