c1a1b19292d16d0935d7295219c9745ae35593cd
777uliahshafar <777uliahshafar@gmail.com>
2024-11-13 19:51:22 +0800

    assetcollecting

---
+++ psscript/assetscollecting.ps1
@@ -0,0 +1,24 @@
+# run to stop script: Unregister-Event FileCreated
+$folder = 'D:\projectfiles' # Enter the root path you want to monitor.
+$filter = ''  # You can enter a wildcard filter here.
+
+$fsw = New-Object IO.FileSystemWatcher $folder, $filter -Property @{IncludeSubdirectories = $false;NotifyFilter = [IO.NotifyFilters]'FileName, LastWrite'}
+
+Register-ObjectEvent $fsw Created -SourceIdentifier FileCreated -Action {
+
+$source = "D:\projectfiles\*" # it should include \*
+$destination = "D:\projectfiles\assets"
+$format = '*.txt', '*.docx'
+Write-Host "Moving all files in '$($source)' to '$($destination)'"
+Get-ChildItem -Path $source -File -Include $format | ForEach-Object {
+    $destPath = Join-Path -Path $destination -ChildPath $_.Name
+    if (Test-Path $destPath) {
+        $counter = 1
+        while (Test-Path $destPath) {
+            $destPath = Join-Path -Path $destination -ChildPath (“{0}-{1}{2}” -f $_.BaseName, $counter, $_.Extension)
+            $counter++
+        }
+    }
+    Move-Item -Path $_.FullName -Destination $destPath
+}
+}
