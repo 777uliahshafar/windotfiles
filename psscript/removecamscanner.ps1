2a398d324632d003cb5d5d202cb961f314d9b1df
777uliahshafar <777uliahshafar@gmail.com>
2024-11-17 16:09:48 +0800

    removecamscanner

---
+++ psscript/removecamscanner.ps1
@@ -0,0 +1,12 @@
+$path = "C:\Users\uliah\Documents\yos\"
+
+cd ${path}
+mkdir ./crop
+magick mogrify -gravity South -chop 0x72 -rotate "+90>" -path ./crop *.jpg
+
+Write-Host -NoNewLine 'Press any key to generate pdf...';
+$null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
+
+cd crop
+magick *.jpg -compress JPEG -quality 90 -page A4 -density 72 -set filename:f "%t-paper" '%[filename:f].pdf'
+
