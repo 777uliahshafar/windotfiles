4d44813556f48144bbfd9c5d8ea09ea2a1f17ab7
777uliahshafar <777uliahshafar@gmail.com>
2024-11-19 05:31:35 +0800

    removecamscannerquality

--- psscript/removecamscanner.ps1
+++ psscript/removecamscanner.ps1
@@ -1,4 +1,4 @@
-. .\"variableonlytweakme.ps1"
+. (Join-Path $PSScriptRoot Variable.ps1)
 
 cd ${rmpath}
 mkdir ./crop
@@ -8,5 +8,5 @@ Write-Host -NoNewLine 'Press any key to generate pdf...';
 $null = $Host.UI.RawUI.ReadKey('NoEcho,IncludeKeyDown');
 
 cd crop
-magick *.jpg -compress JPEG -quality 90 -page A4 -density 72 -set filename:f "%t-paper" '%[filename:f].pdf'
+magick *.jpg -page A4 -strip -kuwahara "1.5" -modulate "100,90" -level "5,92%,0.74" -set filename:f "%t-paper" '%[filename:f].pdf'
 
