78e6b188aeaf772d1651c9d9658d449b7c5f4e97
777uliahshafar <777uliahshafar@gmail.com>
2024-12-04 10:33:55 +0800

    magickout

--- powershell/aliases.ps1
+++ powershell/aliases.ps1
@@ -29,6 +29,17 @@ function obsidian
     & 'nvim' '.\1714384690-MAIN.md'
 }
 
+function compressimghere
+{
+magick *.jpg -sampling-factor 4:2:0 -strip -quality 80 -interlace JPEG -colorspace sRGB -set filename:f "%t-compressed" '%[filename:f].jpg' && magick *.jpeg -sampling-factor 4:2:0 -strip -quality 80 -interlace JPEG -colorspace sRGB -set filename:f "%t-compressed" '%[filename:f].jpg' && magick *.png -sampling-factor 4:2:0 -strip -quality 80 -colorspace sRGB -set filename:f "%t-compressed" '%[filename:f].png'
+}
+
+function pdf2jpeghere
+{
+magick *.pdf -density 150 -quality 100 -flatten -sharpen 0x1.0 -set filename:f "%t-converted" '%[filename:f].jpg'
+}
+
+# Other function
 function obsprop
 {
     cd '~/obs'
@@ -40,9 +51,6 @@ function obseng
     cd '~/obs'
     & 'nvim' '.\1719618861-VQGL.md'
 }
-
-
-
 
 function element
 {
@@ -88,6 +96,8 @@ Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
 Set-Alias nc nvimconfig
 Set-Alias na nvimalias
 Set-Alias nh nvimmyhelp
+Set-Alias compressimg compressimghere
+Set-Alias pdf2img pdf2jpeghere
 
 # Set-Alias phd phdjournal
 Set-Alias st statistika
@@ -100,4 +110,3 @@ Set-Alias as aasessay
 Set-Alias ka karakter
 Set-Alias vp obsprop
 Set-Alias ve obseng
-
--- psscript/pdf2jpeg.ps1
+++
@@ -1,8 +0,0 @@
-. (Join-Path $PSScriptRoot Variable.ps1)
-
-
-$files = Get-ChildItem -Path $propath -Exclude *raw*, *rev*, *ver*
-
-
-
-
