fe13137ebf0be6d8b36d0794fc05c9295d25a031
777uliahshafar <777uliahshafar@gmail.com>
2024-12-25 16:05:37 +0800

    docrename

---
+++ psscript/doc-rename.ps1
@@ -0,0 +1,16 @@
+$docFiles = ".*.pdf|.*.jpg|.*.png|.*.jpeg"
+$docpath = "D:\lecture\bkd-2024-1"
+$files = Get-ChildItem -Path $docpath -Exclude *bkd*.pdf, *bkd*.jpg, *bkd*.jpeg, *bkd*.png
+
+foreach ($file in $files) {
+    $name = $file.Name
+    $fullname = $file.FullName
+    $extension = $file.Extension
+    $dir = $file.Directory.Name
+    $CapitalizedName = (Get-Culture).textinfo.tolower($file.BaseName.tolower()) -replace '\W','-'
+    $base = $CapitalizedName
+
+    if ($name -Match $docFiles  ) {
+        Rename-Item $fullname "$base-$dir$extension"
+    }
+}
